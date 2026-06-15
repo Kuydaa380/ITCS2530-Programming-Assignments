46adffecef955bc1e27e0085af42b73eb4071f38
Kuydaa380 <kuydaa380@macomb.edu>
2026-06-14 23:07:09 -0400

    Week 04: Programming Assignment

--- Week 01/Week01_Programming_Assignment/Week01_Programming_Assignment.cpp
+++ Week 01/Week01_Programming_Assignment/Week01_Programming_Assignment.cpp
@@ -1,7 +1,7 @@
 // Andrew Kuyda
 // ITCS 2530
 // Prof. Koss
-// 6/7/2026
+// 6/13/2026
 
 #include <iostream>
 #include <iomanip>
@@ -11,6 +11,12 @@
 using namespace std;
 
 const string REPORTFILE = "report.txt";
+
+const int MENU_VIEW = 1;
+const int MENU_SAVE = 2;
+
+const int MENU_YES = 1;
+const int MENU_NO = 2;
 
 int main() {
 
@@ -50,22 +56,30 @@ int main() {
 	int visitedState = 0;
 
 	cout << "How many states do you already have snow globes from?" << endl;
-	cout << "Enter a number:" << endl << endl;
+	cout << "Enter a number: ";
 	
 	//Invalid input check
-	if (!(cin >> visitedState))
+	//if (!(cin >> visitedState))
+	//{
+	//	cout << "Please enter a valid number" << endl << endl;
+	//	return 1;
+	//}
+
+	//***WEEK 04 - while loop***
+	cin >> visitedState;
+
+	while (visitedState <= 0 || visitedState > numStates)
 	{
-		cout << "Please enter a valid number" << endl;
-		return 1;
+		cout << "Please enter the number of states you have visited: " << endl;
+		cin >> visitedState;
 	}
-		
 
 	//Output statement based on previous user inputs
-	cout << "So far we've ascertained that your favorite snow globe is from " << favGlobe << ", and that you're most likely to travel to " << bestDest << " next." << endl << endl;
+	cout << endl << "So far we've ascertained that your favorite snow globe is from " << favGlobe << ", and that you're most likely to travel to " << bestDest << " next." << endl;
 
-	cout << "That means that out of " << numStates << " States you've visited " << visitedState << ", which leaves " << numStates - visitedState << " States to visit, or " << numStates - ++visitedState << " after you visit " << bestDest << endl << endl;
+	cout << "That means that out of " << numStates << " States you've visited " << visitedState << ", which leaves " << numStates - visitedState << " States to visit, or " << numStates - visitedState + 1 << " after you visit " << bestDest << "." << endl << endl;
 
-	//***WEEK 03 - If\Else statements***
+	//If\Else statements resulting in string
 
 	if (visitedState >= 1 && visitedState <= 15)
 		cout << "You've barely travelled at all! Better make some travel plans!" << endl << endl;
@@ -77,68 +91,94 @@ int main() {
 	else
 		cout << "Your favorite globe isn't from Michigan?! You have poor taste in snow globes!" << endl << endl;
 
-	//***WEEK 03 - Menu implemented with switch***
+	//***WEEK 04 for loop***
+	cout << "This is your current progress towards your collections completion!" << endl << endl;
+
+	cout << "Progress: [";
+	for (int colProg = 0; colProg < numStates; colProg++)
+	{
+		if (colProg < visitedState)
+			cout << "#";
+		else
+			cout << "_";
+	}
+	cout << "] " << visitedState << "/" << numStates << endl << endl;
+
+	//***WEEK 04 - do while loop***
 	int menuNum;
-	cout << "Please choose from the following menu options by entering the corresponding number: " << endl;
-	cout << "1: View your summary table." << endl;
-	cout << "2: Save your summary table." << endl << endl;
-	cin >> menuNum;
-	
-	cout << endl;
+	int menuOption = 0;
 
-	switch (menuNum)
+	do
 	{
-	case 1:
-		//Summary Table with left align and set width
-		cout << "#######################################" << endl;
-		cout << "#     Snow Globe Tracking Summary     #" << endl;
-		cout << "#######################################" << endl;
-		cout << left;
-		cout << setw(25) << "Favorite Globe:" << setw(15) << favGlobe << endl;
-		cout << setw(25) << "Next Destination:" << setw(15) << bestDest << endl;
-		cout << setw(25) << "States Collected:" << setw(15) << visitedState << endl;
-		cout << setw(25) << "States Remaining:" << setw(15) << numStates - visitedState << endl;
-		cout << "#######################################";
-		break;
+		cout << "Please choose from the following menu options by entering the corresponding number: " << endl;
+		cout << MENU_VIEW << ": View your summary table." << endl;
+		cout << MENU_SAVE << ": Save your summary table." << endl << endl;
+		cin >> menuNum;
 	
-	case 2:
-		//Output Summary to report file with file check
-	{
-		ofstream fReport;
+		cout << endl;
 
-		fReport.open(REPORTFILE);
-		if (!fReport)
+		switch (menuNum)
 		{
-			cout << "Error opening Report File:" << endl;
-			return 1;
+		case MENU_VIEW:
+			//Summary Table with left align and set width
+			cout << "#######################################" << endl;
+			cout << "#     Snow Globe Tracking Summary     #" << endl;
+			cout << "#######################################" << endl;
+			cout << left;
+			cout << setw(25) << "Favorite Globe:" << setw(15) << favGlobe << endl;
+			cout << setw(25) << "Next Destination:" << setw(15) << bestDest << endl;
+			cout << setw(25) << "States Collected:" << setw(15) << visitedState << endl;
+			cout << setw(25) << "States Remaining:" << setw(15) << numStates - visitedState << endl;
+			cout << "#######################################";
+			break;
+	
+		case MENU_SAVE:
+			//Output Summary to report file with file check
+		{
+			ofstream fReport;
+
+			fReport.open(REPORTFILE);
+			if (!fReport)
+			{
+				cout << "Error opening Report File:" << endl;
+				return 1;
+			}
+
+			fReport << "#######################################" << endl;
+			fReport << "#     Snow Globe Tracking Summary     #" << endl;
+			fReport << "#######################################" << endl;
+			fReport << left;
+			fReport << setw(25) << "Favorite Globe:" << setw(15) << favGlobe << endl;
+			fReport << setw(25) << "Next Destination:" << setw(15) << bestDest << endl;
+			fReport << setw(25) << "States Collected:" << setw(15) << visitedState << endl;
+			fReport << setw(25) << "States Remaining:" << setw(15) << (numStates - visitedState) << endl;
+			fReport << "#######################################" << endl;
+
+			fReport.close();
+
+			cout << "Summary saved!" << endl;
+			break;
 		}
 
-		fReport << "#######################################" << endl;
-		fReport << "#     Snow Globe Tracking Summary     #" << endl;
-		fReport << "#######################################" << endl;
-		fReport << left;
-		fReport << setw(25) << "Favorite Globe:" << setw(15) << favGlobe << endl;
-		fReport << setw(25) << "Next Destination:" << setw(15) << bestDest << endl;
-		fReport << setw(25) << "States Collected:" << setw(15) << visitedState << endl;
-		fReport << setw(25) << "States Remaining:" << setw(15) << (numStates - visitedState) << endl;
-		fReport << "#######################################" << endl;
+		default:
+			cout << "Invalid menu option chosen." << endl;
 
-		fReport.close();
+		}
 
-		cout << "Summary saved!" << endl;
-		break;
-	}
+		cout << endl;
+		cout << "Would you like to return to the menu?" << endl;
+		cout << MENU_YES << ": Type '1' for Yes" << endl;
+		cout << MENU_NO << ": Type '2' for No" << endl;
+		cin >> menuOption;
+		cout << endl;
 
-	default:
-		cout << "Invalid menu option chosen." << endl;
 
-	}
+	} while (menuOption == MENU_YES);
 
 	cout << endl << endl << "Exiting menu" << endl;
-
 	
 	return 0;
-}
+	}
 
 
 // State Tracker: Michigan, Ohio, Indiana, Pennsylvania, 
